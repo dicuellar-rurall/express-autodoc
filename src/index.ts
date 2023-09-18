961f142ef0277e7dc7e9305d7eff77677faff5bd
dicuellar <dasencio@rurall.com.co>
2023-09-17 20:12:59 -0500

    v0.0.7

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,15 @@
 import express from 'express';
 import ExpressParser from './parser';
 import SwaggerGenerator from './generator';
-import swaggerUi from 'swagger-ui-express';
 
-export default function setupSwaggerUI(app: express.Application) {
+function generateSwaggerSpec(app: express.Application) {
   const parser = new ExpressParser(app);
   parser.parse();
 
   const generator = new SwaggerGenerator(parser.getRoutes());
   generator.generate();
 
-  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(generator.getSpec()));
+  return generator.getSpec();
 }
 
+export default generateSwaggerSpec;
